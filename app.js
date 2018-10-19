46d2377e9283e29b8b526088289444616d477cd6
Navdeep Singh <navdeepsingham@gmail.com>
2018-10-19 17:18:14 +0530

    references page added

--- app.js
+++ app.js
@@ -86,6 +86,13 @@ app.get("/quickNotes", function(req, res) {
     else
         res.redirect('./');
 });
+app.get("/references", function(req, res) {
+    if (req.session.uid)
+        res.render("references",{uname: req.session.uid,pageHeading: "REFERENCES"});
+    else
+        res.redirect('./');
+});
+
 
 app.get("/quiz", function(req, res) {
     if (req.session.uid)
